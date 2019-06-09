1c50d665db9f6b7a30c4ce86e2aee2e21a91eca4
白雪 <baixue@baixuedeMacBook-Pro.local>
2019-06-09 22:20:54 +0800

    加入mock

---
+++ mock/dashboard_chart.js
@@ -0,0 +1,14 @@
+function chart(method) {
+  let res = null
+  switch (method) {
+    case 'GET':
+      res = [20, 40, 78, 10, 30, 58]
+      break
+    default:
+      res = null
+  }
+
+  return res
+}
+
+module.exports = chart
--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,25 @@ module.exports = {
         javascriptEnabled: true
       }
     }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        bypass: function(req, res) {
+          if (req.headers.accept.indexOf('html') !== -1) {
+            console.log('Skipping proxy for browser request.')
+            return '/index.html'
+          } else {
+            console.log(req.path)
+            const name = req.path.split('/api/').split('/').join('_')
+            const mock = require(`./mock/${name}`)
+            const result = mock(req.method)
+            delete require.cache[require.resolve(`./mock/${name}`)]
+            return res.send(result)
+          }
+        }
+      }
+    }
   }
 };
